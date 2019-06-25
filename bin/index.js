7c1ed5effb6e6bbc9d135d3c83119d105e896bc0
xeroblood <xeroblood@owntheday.io>
2019-06-25 17:53:33 -0400

    Minor updates to contracts

--- bin/index.js
+++ bin/index.js
@@ -3,13 +3,16 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-exports.CryptoCardsHelpers = exports.CryptoCardsController = undefined;
+exports.CryptoCardsHelpers = exports.CryptoCardsController = exports.CryptoCardsLib = undefined;
 
 require('babel-polyfill');
 
 var _helpers = require('./helpers');
 
+var _cryptoCardsLib = require('./crypto-cards-lib');
+
 var _cryptoCardsController = require('./crypto-cards-controller');
 
+exports.CryptoCardsLib = _cryptoCardsLib.CryptoCardsLib;
 exports.CryptoCardsController = _cryptoCardsController.CryptoCardsController;
 exports.CryptoCardsHelpers = _helpers.CryptoCardsHelpers;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import 'babel-polyfill';
 
 import { CryptoCardsHelpers } from './helpers';
+import { CryptoCardsLib } from './crypto-cards-lib';
 import { CryptoCardsController } from './crypto-cards-controller';
 
 export {
+    CryptoCardsLib,
     CryptoCardsController,
     CryptoCardsHelpers
 }
