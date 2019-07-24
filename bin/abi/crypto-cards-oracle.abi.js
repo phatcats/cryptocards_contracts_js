c35c50bccf782cb69d78b229b92fe17178340916
xeroblood <xeroblood@owntheday.io>
2019-07-24 17:47:17 -0400

    Minor updates to contracts

--- bin/abi/crypto-cards-oracle.abi.js
+++ bin/abi/crypto-cards-oracle.abi.js
@@ -1,233 +1,237 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
 var CryptoCardsOracleABI = exports.CryptoCardsOracleABI = [{
-    'constant': false,
-    'inputs': [],
-    'name': 'renounceOwnership',
-    'outputs': [],
-    'payable': false,
-    'stateMutability': 'nonpayable',
-    'type': 'function'
+    "constant": false,
+    "inputs": [],
+    "name": "renounceOwnership",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
 }, {
-    'constant': true,
-    'inputs': [],
-    'name': 'owner',
-    'outputs': [{
-        'name': '',
-        'type': 'address'
+    "constant": true,
+    "inputs": [],
+    "name": "owner",
+    "outputs": [{
+        "name": "",
+        "type": "address"
     }],
-    'payable': false,
-    'stateMutability': 'view',
-    'type': 'function'
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
 }, {
-    'constant': true,
-    'inputs': [],
-    'name': 'isOwner',
-    'outputs': [{
-        'name': '',
-        'type': 'bool'
+    "constant": true,
+    "inputs": [],
+    "name": "isOwner",
+    "outputs": [{
+        "name": "",
+        "type": "bool"
     }],
-    'payable': false,
-    'stateMutability': 'view',
-    'type': 'function'
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
 }, {
-    'constant': false,
-    'inputs': [{
-        'name': 'newOwner',
-        'type': 'address'
+    "constant": false,
+    "inputs": [{
+        "name": "newOwner",
+        "type": "address"
     }],
-    'name': 'transferOwnership',
-    'outputs': [],
-    'payable': false,
-    'stateMutability': 'nonpayable',
-    'type': 'function'
+    "name": "transferOwnership",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
 }, {
-    'anonymous': false,
-    'inputs': [{
-        'indexed': true,
-        'name': 'receiver',
-        'type': 'address'
+    "anonymous": false,
+    "inputs": [{
+        "indexed": true,
+        "name": "receiver",
+        "type": "address"
     }, {
-        'indexed': false,
-        'name': 'uuid',
-        'type': 'bytes16'
+        "indexed": false,
+        "name": "uuid",
+        "type": "bytes16"
     }, {
-        'indexed': false,
-        'name': 'packId',
-        'type': 'uint256'
-    }],
-    'name': 'ReceivedNewPack',
-    'type': 'event'
-}, {
-    'anonymous': false,
-    'inputs': [{
-        'indexed': true,
-        'name': 'receiver',
-        'type': 'address'
+        "indexed": false,
+        "name": "packId",
+        "type": "uint256"
     }, {
-        'indexed': false,
-        'name': 'uuid',
-        'type': 'bytes16'
+        "indexed": false,
+        "name": "packData",
+        "type": "string"
+    }],
+    "name": "ReceivedNewPack",
+    "type": "event"
+}, {
+    "anonymous": false,
+    "inputs": [{
+        "indexed": true,
+        "name": "receiver",
+        "type": "address"
     }, {
-        'indexed': false,
-        'name': 'errorCode',
-        'type': 'uint8'
-    }],
-    'name': 'PackError',
-    'type': 'event'
-}, {
-    'anonymous': false,
-    'inputs': [{
-        'indexed': true,
-        'name': 'previousOwner',
-        'type': 'address'
+        "indexed": false,
+        "name": "uuid",
+        "type": "bytes16"
     }, {
-        'indexed': true,
-        'name': 'newOwner',
-        'type': 'address'
+        "indexed": false,
+        "name": "errorCode",
+        "type": "uint8"
     }],
-    'name': 'OwnershipTransferred',
-    'type': 'event'
+    "name": "PackError",
+    "type": "event"
 }, {
-    'constant': false,
-    'inputs': [{
-        'name': '_owner',
-        'type': 'address'
-    }],
-    'name': 'initialize',
-    'outputs': [],
-    'payable': false,
-    'stateMutability': 'nonpayable',
-    'type': 'function'
-}, {
-    'constant': true,
-    'inputs': [{
-        'name': '_uuid',
-        'type': 'bytes16'
-    }],
-    'name': 'isValidUuid',
-    'outputs': [{
-        'name': '',
-        'type': 'bool'
-    }],
-    'payable': false,
-    'stateMutability': 'view',
-    'type': 'function'
-}, {
-    'constant': false,
-    'inputs': [],
-    'name': 'getGasReserve',
-    'outputs': [{
-        'name': '',
-        'type': 'uint256'
-    }],
-    'payable': false,
-    'stateMutability': 'nonpayable',
-    'type': 'function'
-}, {
-    'constant': false,
-    'inputs': [{
-        'name': 'receiver',
-        'type': 'address'
+    "anonymous": false,
+    "inputs": [{
+        "indexed": true,
+        "name": "previousOwner",
+        "type": "address"
     }, {
-        'name': 'gasReserve',
-        'type': 'uint256'
+        "indexed": true,
+        "name": "newOwner",
+        "type": "address"
+    }],
+    "name": "OwnershipTransferred",
+    "type": "event"
+}, {
+    "constant": false,
+    "inputs": [{
+        "name": "_owner",
+        "type": "address"
+    }],
+    "name": "initialize",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "constant": true,
+    "inputs": [{
+        "name": "_uuid",
+        "type": "bytes16"
+    }],
+    "name": "isValidUuid",
+    "outputs": [{
+        "name": "",
+        "type": "bool"
+    }],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [],
+    "name": "getGasReserve",
+    "outputs": [{
+        "name": "",
+        "type": "uint256"
+    }],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [{
+        "name": "receiver",
+        "type": "address"
     }, {
-        'name': 'uuid',
-        'type': 'bytes16'
-    }],
-    'name': 'getNewPack',
-    'outputs': [],
-    'payable': true,
-    'stateMutability': 'payable',
-    'type': 'function'
-}, {
-    'constant': false,
-    'inputs': [{
-        'name': '_myid',
-        'type': 'bytes32'
+        "name": "gasReserve",
+        "type": "uint256"
     }, {
-        'name': '_result',
-        'type': 'string'
+        "name": "uuid",
+        "type": "bytes16"
+    }],
+    "name": "getNewPack",
+    "outputs": [],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [{
+        "name": "_myid",
+        "type": "bytes32"
     }, {
-        'name': '_proof',
-        'type': 'bytes'
-    }],
-    'name': '__callback',
-    'outputs': [],
-    'payable': false,
-    'stateMutability': 'nonpayable',
-    'type': 'function'
-}, {
-    'constant': false,
-    'inputs': [{
-        'name': '_queryId',
-        'type': 'bytes32'
+        "name": "_result",
+        "type": "string"
     }, {
-        'name': '_result',
-        'type': 'string'
+        "name": "_proof",
+        "type": "bytes"
     }],
-    'name': '__callback',
-    'outputs': [],
-    'payable': false,
-    'stateMutability': 'nonpayable',
-    'type': 'function'
+    "name": "__callback",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
 }, {
-    'constant': false,
-    'inputs': [{
-        'name': '_controller',
-        'type': 'address'
+    "constant": false,
+    "inputs": [{
+        "name": "_queryId",
+        "type": "bytes32"
+    }, {
+        "name": "_result",
+        "type": "string"
     }],
-    'name': 'setContractController',
-    'outputs': [],
-    'payable': false,
-    'stateMutability': 'nonpayable',
-    'type': 'function'
+    "name": "__callback",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
 }, {
-    'constant': false,
-    'inputs': [{
-        'name': '_packs',
-        'type': 'address'
+    "constant": false,
+    "inputs": [{
+        "name": "_controller",
+        "type": "address"
     }],
-    'name': 'setPacksAddress',
-    'outputs': [],
-    'payable': false,
-    'stateMutability': 'nonpayable',
-    'type': 'function'
+    "name": "setContractController",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
 }, {
-    'constant': false,
-    'inputs': [{
-        'name': '_endpoint',
-        'type': 'string'
+    "constant": false,
+    "inputs": [{
+        "name": "_packs",
+        "type": "address"
     }],
-    'name': 'updateApiEndpoint',
-    'outputs': [],
-    'payable': false,
-    'stateMutability': 'nonpayable',
-    'type': 'function'
+    "name": "setPacksAddress",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
 }, {
-    'constant': false,
-    'inputs': [{
-        'name': '_wei',
-        'type': 'uint256'
+    "constant": false,
+    "inputs": [{
+        "name": "_endpoint",
+        "type": "string"
     }],
-    'name': 'updateOracleGasPrice',
-    'outputs': [],
-    'payable': true,
-    'stateMutability': 'payable',
-    'type': 'function'
+    "name": "updateApiEndpoint",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
 }, {
-    'constant': false,
-    'inputs': [{
-        'name': '_wei',
-        'type': 'uint256'
+    "constant": false,
+    "inputs": [{
+        "name": "_wei",
+        "type": "uint256"
     }],
-    'name': 'updateOracleGasLimit',
-    'outputs': [],
-    'payable': false,
-    'stateMutability': 'nonpayable',
-    'type': 'function'
+    "name": "updateOracleGasPrice",
+    "outputs": [],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [{
+        "name": "_wei",
+        "type": "uint256"
+    }],
+    "name": "updateOracleGasLimit",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
 }];
--- src/abi/crypto-cards-oracle.abi.js
+++ src/abi/crypto-cards-oracle.abi.js
@@ -1,293 +1,298 @@
 export const CryptoCardsOracleABI = [
     {
-        'constant': false,
-        'inputs': [],
-        'name': 'renounceOwnership',
-        'outputs': [],
-        'payable': false,
-        'stateMutability': 'nonpayable',
-        'type': 'function'
+        "constant": false,
+        "inputs": [],
+        "name": "renounceOwnership",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
     },
     {
-        'constant': true,
-        'inputs': [],
-        'name': 'owner',
-        'outputs': [
+        "constant": true,
+        "inputs": [],
+        "name": "owner",
+        "outputs": [
             {
-                'name': '',
-                'type': 'address'
+                "name": "",
+                "type": "address"
             }
         ],
-        'payable': false,
-        'stateMutability': 'view',
-        'type': 'function'
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
     },
     {
-        'constant': true,
-        'inputs': [],
-        'name': 'isOwner',
-        'outputs': [
+        "constant": true,
+        "inputs": [],
+        "name": "isOwner",
+        "outputs": [
             {
-                'name': '',
-                'type': 'bool'
+                "name": "",
+                "type": "bool"
             }
         ],
-        'payable': false,
-        'stateMutability': 'view',
-        'type': 'function'
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
     },
     {
-        'constant': false,
-        'inputs': [
+        "constant": false,
+        "inputs": [
             {
-                'name': 'newOwner',
-                'type': 'address'
+                "name": "newOwner",
+                "type": "address"
             }
         ],
-        'name': 'transferOwnership',
-        'outputs': [],
-        'payable': false,
-        'stateMutability': 'nonpayable',
-        'type': 'function'
+        "name": "transferOwnership",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
     },
     {
-        'anonymous': false,
-        'inputs': [
+        "anonymous": false,
+        "inputs": [
             {
-                'indexed': true,
-                'name': 'receiver',
-                'type': 'address'
+                "indexed": true,
+                "name": "receiver",
+                "type": "address"
             },
             {
-                'indexed': false,
-                'name': 'uuid',
-                'type': 'bytes16'
+                "indexed": false,
+                "name": "uuid",
+                "type": "bytes16"
             },
             {
-                'indexed': false,
-                'name': 'packId',
-                'type': 'uint256'
-            }
-        ],
-        'name': 'ReceivedNewPack',
-        'type': 'event'
-    },
-    {
-        'anonymous': false,
-        'inputs': [
-            {
-                'indexed': true,
-                'name': 'receiver',
-                'type': 'address'
+                "indexed": false,
+                "name": "packId",
+                "type": "uint256"
             },
             {
-                'indexed': false,
-                'name': 'uuid',
-                'type': 'bytes16'
+                "indexed": false,
+                "name": "packData",
+                "type": "string"
+            }
+        ],
+        "name": "ReceivedNewPack",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "name": "receiver",
+                "type": "address"
             },
             {
-                'indexed': false,
-                'name': 'errorCode',
-                'type': 'uint8'
-            }
-        ],
-        'name': 'PackError',
-        'type': 'event'
-    },
-    {
-        'anonymous': false,
-        'inputs': [
-            {
-                'indexed': true,
-                'name': 'previousOwner',
-                'type': 'address'
+                "indexed": false,
+                "name": "uuid",
+                "type": "bytes16"
             },
             {
-                'indexed': true,
-                'name': 'newOwner',
-                'type': 'address'
+                "indexed": false,
+                "name": "errorCode",
+                "type": "uint8"
             }
         ],
-        'name': 'OwnershipTransferred',
-        'type': 'event'
+        "name": "PackError",
+        "type": "event"
     },
     {
-        'constant': false,
-        'inputs': [
+        "anonymous": false,
+        "inputs": [
             {
-                'name': '_owner',
-                'type': 'address'
-            }
-        ],
-        'name': 'initialize',
-        'outputs': [],
-        'payable': false,
-        'stateMutability': 'nonpayable',
-        'type': 'function'
-    },
-    {
-        'constant': true,
-        'inputs': [
-            {
-                'name': '_uuid',
-                'type': 'bytes16'
-            }
-        ],
-        'name': 'isValidUuid',
-        'outputs': [
-            {
-                'name': '',
-                'type': 'bool'
-            }
-        ],
-        'payable': false,
-        'stateMutability': 'view',
-        'type': 'function'
-    },
-    {
-        'constant': false,
-        'inputs': [],
-        'name': 'getGasReserve',
-        'outputs': [
-            {
-                'name': '',
-                'type': 'uint256'
-            }
-        ],
-        'payable': false,
-        'stateMutability': 'nonpayable',
-        'type': 'function'
-    },
-    {
-        'constant': false,
-        'inputs': [
-            {
-                'name': 'receiver',
-                'type': 'address'
+                "indexed": true,
+                "name": "previousOwner",
+                "type": "address"
             },
             {
-                'name': 'gasReserve',
-                'type': 'uint256'
+                "indexed": true,
+                "name": "newOwner",
+                "type": "address"
+            }
+        ],
+        "name": "OwnershipTransferred",
+        "type": "event"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_owner",
+                "type": "address"
+            }
+        ],
+        "name": "initialize",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "_uuid",
+                "type": "bytes16"
+            }
+        ],
+        "name": "isValidUuid",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [],
+        "name": "getGasReserve",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "receiver",
+                "type": "address"
             },
             {
-                'name': 'uuid',
-                'type': 'bytes16'
-            }
-        ],
-        'name': 'getNewPack',
-        'outputs': [],
-        'payable': true,
-        'stateMutability': 'payable',
-        'type': 'function'
-    },
-    {
-        'constant': false,
-        'inputs': [
-            {
-                'name': '_myid',
-                'type': 'bytes32'
+                "name": "gasReserve",
+                "type": "uint256"
             },
             {
-                'name': '_result',
-                'type': 'string'
+                "name": "uuid",
+                "type": "bytes16"
+            }
+        ],
+        "name": "getNewPack",
+        "outputs": [],
+        "payable": true,
+        "stateMutability": "payable",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_myid",
+                "type": "bytes32"
             },
             {
-                'name': '_proof',
-                'type': 'bytes'
-            }
-        ],
-        'name': '__callback',
-        'outputs': [],
-        'payable': false,
-        'stateMutability': 'nonpayable',
-        'type': 'function'
-    },
-    {
-        'constant': false,
-        'inputs': [
-            {
-                'name': '_queryId',
-                'type': 'bytes32'
+                "name": "_result",
+                "type": "string"
             },
             {
-                'name': '_result',
-                'type': 'string'
+                "name": "_proof",
+                "type": "bytes"
             }
         ],
-        'name': '__callback',
-        'outputs': [],
-        'payable': false,
-        'stateMutability': 'nonpayable',
-        'type': 'function'
+        "name": "__callback",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
     },
     {
-        'constant': false,
-        'inputs': [
+        "constant": false,
+        "inputs": [
             {
-                'name': '_controller',
-                'type': 'address'
+                "name": "_queryId",
+                "type": "bytes32"
+            },
+            {
+                "name": "_result",
+                "type": "string"
             }
         ],
-        'name': 'setContractController',
-        'outputs': [],
-        'payable': false,
-        'stateMutability': 'nonpayable',
-        'type': 'function'
+        "name": "__callback",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
     },
     {
-        'constant': false,
-        'inputs': [
+        "constant": false,
+        "inputs": [
             {
-                'name': '_packs',
-                'type': 'address'
+                "name": "_controller",
+                "type": "address"
             }
         ],
-        'name': 'setPacksAddress',
-        'outputs': [],
-        'payable': false,
-        'stateMutability': 'nonpayable',
-        'type': 'function'
+        "name": "setContractController",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
     },
     {
-        'constant': false,
-        'inputs': [
+        "constant": false,
+        "inputs": [
             {
-                'name': '_endpoint',
-                'type': 'string'
+                "name": "_packs",
+                "type": "address"
             }
         ],
-        'name': 'updateApiEndpoint',
-        'outputs': [],
-        'payable': false,
-        'stateMutability': 'nonpayable',
-        'type': 'function'
+        "name": "setPacksAddress",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
     },
     {
-        'constant': false,
-        'inputs': [
+        "constant": false,
+        "inputs": [
             {
-                'name': '_wei',
-                'type': 'uint256'
+                "name": "_endpoint",
+                "type": "string"
             }
         ],
-        'name': 'updateOracleGasPrice',
-        'outputs': [],
-        'payable': true,
-        'stateMutability': 'payable',
-        'type': 'function'
+        "name": "updateApiEndpoint",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
     },
     {
-        'constant': false,
-        'inputs': [
+        "constant": false,
+        "inputs": [
             {
-                'name': '_wei',
-                'type': 'uint256'
+                "name": "_wei",
+                "type": "uint256"
             }
         ],
-        'name': 'updateOracleGasLimit',
-        'outputs': [],
-        'payable': false,
-        'stateMutability': 'nonpayable',
-        'type': 'function'
+        "name": "updateOracleGasPrice",
+        "outputs": [],
+        "payable": true,
+        "stateMutability": "payable",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_wei",
+                "type": "uint256"
+            }
+        ],
+        "name": "updateOracleGasLimit",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
     }
 ];
